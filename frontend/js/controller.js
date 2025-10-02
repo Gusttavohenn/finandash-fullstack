8e69bb311f7775bca1ca976d3f543ff6ce0f689f
Gustavo Henrique de Oliveira <gustavo.oliveira@minasgusa.local>
2025-10-02 16:16:13 -0300

    Favicon

--- frontend/js/controller.js
+++ frontend/js/controller.js
@@ -113,15 +113,8 @@ function setFavicon() {
     const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
     link.type = 'image/svg+xml';
     link.rel = 'shortcut icon';
-
-    const svg = `
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-            <path fill="#4A69BD" d="M461.2 128.3L371.3 218c-3.9 3.9-10.2 3.9-14.1 0l-42.2-42.2c-3.9-3.9-3.9-10.2 0-14.1l89.9-89.9c3.9-3.9 10.2-3.9 14.1 0l42.2 42.2c3.9 3.9 3.9 10.2 0 14.1zM315.9 272.5L200 388.4l-42.2-42.2c-3.9-3.9-10.2-3.9-14.1 0L53.7 436.3c-3.9 3.9-3.9 10.2 0 14.1l42.2 42.2c3.9 3.9 10.2 3.9 14.1 0L200 404.7l115.9 115.9c3.9 3.9 10.2 3.9 14.1 0l42.2-42.2c3.9-3.9 3.9-10.2 0-14.1L279.8 286.6c-3.9-3.9-10.2-3.9-14.1 0l-42.2 42.2c-3.9 3.9-10.2 3.9-14.1 0L157.3 279c-3.9-3.9-3.9-10.2 0-14.1l42.2-42.2c3.9-3.9 10.2-3.9 14.1 0l115.9 115.9c3.9 3.9 10.2 3.9 14.1 0l42.2-42.2c3.9-3.9 3.9-10.2 0-14.1L315.9 272.5zM48 64C21.5 64 0 85.5 0 112v288c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48z"/>
-        </svg>
-    `;
-    
+    const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="#4A69BD" d="M64 32C28.7 32 0 60.7 0 96v320c0 35.3 28.7 64 64 64h448c35.3 0 64-28.7 64-64V192c0-35.3-28.7-64-64-64H80c-8.8 0-16-7.2-16-16s7.2-16 16-16h368c17.7 0 32-14.3 32-32s-14.3-32-32-32H64zM80 256h384c8.8 0 16 7.2 16 16v160c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V272c0-8.8 7.2-16 16-16z"/></svg>`;
     link.href = `data:image/svg+xml,${encodeURIComponent(svg)}`;
-    document.getElementsByTagName('head')[0].appendChild(link);
+    document.head.appendChild(link);
 }
-
 setFavicon();
